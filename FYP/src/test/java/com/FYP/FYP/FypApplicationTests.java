66e94daff876a0eec0e1c9d094d5665761953ce5
daniellimotto <daniellimotto10@gmail.com>
2025-01-22 17:49:09 +0800

    Initial commit

---
+++ FYP/src/test/java/com/FYP/FYP/FypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FYP.FYP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
